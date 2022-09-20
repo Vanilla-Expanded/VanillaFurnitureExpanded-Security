455646e3a1c952327f1d617cc9f56c55ff5b165b
KylianB <kylian.balde@gmail.com>
2022-09-20 15:08:54 +0200

    remove psychic pylon

--- Source/VFESecurity/Defs/ThingDefOf.cs
+++ Source/VFESecurity/Defs/ThingDefOf.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Text;
-using UnityEngine;
+﻿using RimWorld;
 using Verse;
-using Verse.AI;
-using Verse.AI.Group;
-using RimWorld;
-using HarmonyLib;
 
 namespace VFESecurity
 {
-
     [DefOf]
     public static class ThingDefOf
     {
-
         public static ThingDef VFES_ArtilleryStrikeIncoming;
         public static ThingDef VFES_ArtilleryStrikeLeaving;
         public static ThingDef VFES_ActiveArtilleryStrike;
@@ -26,14 +14,11 @@ namespace VFESecurity
 
         public static ThingDef VFES_Turret_Artillery;
         public static ThingDef VFES_Gun_Searchlight;
-        public static ThingDef VFES_PsychicPylon;
         public static ThingDef VFES_AmmoCrate;
 
         public static ThingDef VFES_Artillery_Rock;
 
         // Motes
         public static ThingDef VFES_Mote_SpotLight;
-
     }
-
 }
