444aee36b4d72174b49ed2e3be21d5144d6d7580
SokyranTheDragon <exploder1531@gmail.com>
2026-01-03 17:54:34 +0100

    Shockmine extra damage

---
+++ 1.6/Source/Comps/CompProperties_ExplosiveExtraDamage.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VFESecurity;
+
+public class CompProperties_ExplosiveExtraDamage : CompProperties_Explosive
+{
+    public DamageDef extraExplosiveDamageType;
+    public int extraDamageAmountBase;
+    public float extraArmorPenetrationBase;
+    public float extraChanceToStartFire;
+    public bool extraDamageFalloff;
+}
---
+++ 1.6/Source/HarmonyPatches/CompExplosive_Detonate_Patch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFESecurity;
+
+[HarmonyPatch(typeof(CompExplosive), nameof(CompExplosive.Detonate))]
+public class CompExplosive_Detonate_Patch
+{
+    // Consider making this conditional, in case some mod removes those CompProps and/or the shockmine?
+
+    private static void Prefix(CompExplosive __instance, Map map, bool ignoreUnspawned)
+    {
+        if (__instance.Props is not CompProperties_ExplosiveExtraDamage props)
+            return;
+
+        if (map == null)
+            return;
+
+        if (!ignoreUnspawned && !__instance.parent.SpawnedOrAnyParentSpawned)
+            return;
+
+        var radius = __instance.ExplosiveRadius();
+        if (radius <= 0)
+            return;
+
+        var parent = __instance.instigator != null && (!__instance.instigator.HostileTo(__instance.parent.Faction) || __instance.parent.Faction == Faction.OfPlayer)
+            ? __instance.instigator
+            : __instance.parent;
+
+        GenExplosion.DoExplosion(parent.PositionHeld, map, radius, props.extraExplosiveDamageType, parent, props.extraDamageAmountBase, props.extraArmorPenetrationBase,
+            applyDamageToExplosionCellsNeighbors: props.applyDamageToExplosionCellsNeighbors, damageFalloff: props.extraDamageFalloff, chanceToStartFire: props.extraChanceToStartFire,
+            ignoredThings: __instance.thingsIgnoredByExplosion, doSoundEffects: false, doVisualEffects: false);
+    }
+}
