449dff27f41e201ce643ed4fe1ff1a5ea4e93d14
SokyranTheDragon <exploder1531@gmail.com>
2026-01-08 23:29:29 +0100

    Forgot a file

---
+++ 1.6/Source/Things/Building_TurretSearchlight.cs
@@ -0,0 +1,39 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VFESecurity;
+
+public class Building_TurretSearchlight : Building_TurretGun
+{
+    public override void SpawnSetup(Map map, bool respawningAfterLoad)
+    {
+        base.SpawnSetup(map, respawningAfterLoad);
+
+        if (respawningAfterLoad && CurrentTarget.HasThing && AttackVerb is Verb_Dazzle dazzle && dazzle.CanHitTarget(CurrentTarget))
+            dazzle.TryCastShot();
+    }
+
+    public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+    {
+        base.DeSpawn(mode);
+
+        if (AttackVerb is Verb_Dazzle dazzle)
+            dazzle.Cleanup();
+    }
+
+    public override void DrawExtraSelectionOverlays()
+    {
+        var warmupTicks = burstWarmupTicksLeft;
+        if (!AttackVerb.verbProps.drawAimPie)
+            burstWarmupTicksLeft = 0;
+
+        try
+        {
+            base.DrawExtraSelectionOverlays();
+        }
+        finally
+        {
+            burstWarmupTicksLeft = warmupTicks;
+        }
+    }
+}
