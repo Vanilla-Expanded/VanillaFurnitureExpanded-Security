204070f66389b4bd4c20f0728b165542780db815
SokyranTheDragon <exploder1531@gmail.com>
2026-05-20 17:11:11 +0200

    Fix concealed buildings setting all pathfinding costs to 0 on their tiles

--- 1.6/Source/HarmonyPatches/PathGrid_CalculatedCostAt_Patch.cs
+++ 1.6/Source/HarmonyPatches/PathGrid_CalculatedCostAt_Patch.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using HarmonyLib;
 using Verse;
 using Verse.AI;
@@ -7,17 +8,33 @@ namespace VFESecurity
     [HarmonyPatch(typeof(PathGrid), nameof(PathGrid.CalculatedCostAt))]
     public static class PathGrid_CalculatedCostAt_Patch
     {
-        public static void Postfix(PathGrid __instance, ref int __result, IntVec3 c, bool perceivedStatic, IntVec3 prevCell)
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
         {
-            var things = __instance.map.thingGrid.ThingsListAt(c);
-            for (int i = 0; i < things.Count; i++)
-            {
-                if (things[i] is IConcealedBuilding concealed && concealed.ConcealedComp != null && concealed.ConcealedComp.Submerged)
-                {
-                    __result = 0;
-                    return;
-                }
-            }
+            var matcher = new CodeMatcher(instr);
+
+            // Find "thing.def.pathCost;"
+            matcher.MatchEndForward(
+                CodeMatch.LoadsLocal(),
+                CodeMatch.LoadsField(typeof(Thing).DeclaredField(nameof(Thing.def))),
+                CodeMatch.LoadsField(typeof(BuildableDef).DeclaredField(nameof(BuildableDef.pathCost)))
+            );
+
+            // Wrap the "thing.def.pathCost" with our method and insert the Thing argument
+            matcher.InsertAfter(
+                // Clone the instruction loading the Thing argument
+                matcher.InstructionAt(-2).Clone(),
+                CodeInstruction.Call(() => ModifyPathCostIfConcealed)
+            );
+
+            return matcher.Instructions();
+        }
+
+        private static int ModifyPathCostIfConcealed(int pathCost, Thing thing)
+        {
+            // Thing is IConcealedBuilding, has a non-null comp, and is submerged
+            if (thing is IConcealedBuilding { ConcealedComp.Submerged: true })
+                return 0;
+            return pathCost;
         }
     }
 }
